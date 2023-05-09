0d33d49ef0a543e75af0d012e040710533611825
Josep Maria Rocafort <josepsmachine@Joseps-MacBook-Air.local>
2023-05-09 19:33:18 +0200

    playing with the dataset, got weather hourly! :)

---
+++ dataset.py
@@ -0,0 +1,5 @@
+from torch.utils.data import Dataset, DataLoader
+
+class energyProject_dataset(Dataset):
+    def __init__(self,climate_filepath,consumption_filepath,occupation_filepath):
+        pass
