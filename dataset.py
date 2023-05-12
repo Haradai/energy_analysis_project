949de7899353a480d3e03e6eb19e895eae28133a
Josep Maria Rocafort <josepsmachine@Joseps-MacBook-Air.local>
2023-05-12 02:10:08 +0200

    anada de olla dataloader chetao

--- dataset.py
+++
@@ -1,5 +0,0 @@
-from torch.utils.data import Dataset, DataLoader
-
-class energyProject_dataset(Dataset):
-    def __init__(self,climate_filepath,consumption_filepath,occupation_filepath):
-        pass
